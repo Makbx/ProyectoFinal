f9a014c57580ee949df76109a01ec897cad1c5fa
Makbx <129413392+Makbx@users.noreply.github.com>
2023-10-06 12:06:03 -0300

    alo

--- src/AccesoADatos/AlojamientoData.java
+++ src/AccesoADatos/AlojamientoData.java
@@ -4,10 +4,100 @@
  */
 package AccesoADatos;
 
+import java.sql.*;
+import javax.swing.JOptionPane;
+import Entidades.Alojamiento;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author crist
  */
 public class AlojamientoData {
+    private Connection con=null;
+    private CiudadData ciuData;
     
+    public AlojamientoData(){
+        con=Conexion.getConexion();
+        this.ciuData = new CiudadData();
+    }
+        public void guardadAlojamiento(Alojamiento alo){
+        String sql="INSERT INTO alojamiento (idCiudad, fechaInicio, fechaFin, tipo, costoDiario, estado)"
+                + "VALUE(? , ?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1,alo.getCiudad().getIdCiudad());
+            ps.setDate(2,Date.valueOf(alo.getFechaInicio()));
+            ps.setDate(3,Date.valueOf(alo.getFechaFin()));
+            ps.setString(4,alo.getTipo());
+            ps.setDouble(5,alo.getCosto());
+            ps.setBoolean(6, alo.isActivo());
+            ps.executeUpdate();
+
+            ResultSet rs=ps.getGeneratedKeys(); 
+            if(rs.next()){
+                alo.setIdAlojamiento(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Alojamiento Guardado");
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showConfirmDialog(null, "Error al acceder a la tabla Alojamiento");
+        }
+    }
+        public void modificarAlojamiento(Alojamiento alo){
+        String sql="UPDATE alojamiento SET fechaInicio = ?, fechaFin = ?, tipo = ?, costoDiario = ?, estado = ? WHERE idAlojamiento= ? AND idCiudad = ?";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setDate(1,Date.valueOf(alo.getFechaInicio()));
+            ps.setDate(2,Date.valueOf(alo.getFechaFin()));
+            ps.setString(3,alo.getTipo());
+            ps.setDouble(4,alo.getCosto());
+            ps.setBoolean(5, alo.isActivo());
+            int exito = ps.executeUpdate();
+            if(exito==1){
+                JOptionPane.showMessageDialog(null, "Alojamiento modificado");
+            }
+            ps.close();       
+        } catch (SQLException ex) {
+            JOptionPane.showConfirmDialog(null, "Error al acceder a la tabla Alojamiento");
+        }
+    }
+    public void eliminarAlojamiento(int id){ //Borrado logico consiste en poner su estado a 0 no borrarlo
+        String sql="UPDATE alojamiento SET estado = 0 WHERE idAlojamiento =?";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int exito = ps.executeUpdate();
+            if(exito==1){
+                JOptionPane.showMessageDialog(null, "Alojamiento Eliminado");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Alojamiento");
+        }
+    }
+    public List<Alojamiento> listarAlojamientos(){
+        String sql = "SELECT * FROM alojamiento";
+        ArrayList<Alojamiento> alojamientos = new ArrayList<>();
+        Alojamiento alojamiento = null;
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                alojamiento = new Alojamiento();
+                alojamiento.setIdAlojamiento(rs.getInt("idAlojamiento"));
+                alojamiento.setCiudad(ciuData.buscarCiudadPorId(rs.getInt("idCiudad")));
+                alojamiento.setFechaInicio(rs.getDate("fechaInicio").toLocalDate());
+                alojamiento.setFechaFin(rs.getDate("fechaFin").toLocalDate());
+                alojamiento.setEstado(rs.getBoolean("estado"));
+                alojamientos.add(alojamiento);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Alojamiento");
+        }
+        return alojamientos;
+    }  
 }
